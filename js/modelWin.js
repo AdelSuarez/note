7fe36e43a61b187e882160fdf0be0615c8085b42
Adel Suarez <adel90suarez@gmail.com>
2022-01-27 21:01:38 -0400

    popup buttons modularized

---
+++ js/modelWin.js
@@ -0,0 +1,13 @@
+
+export function open (btn, container){
+    btn.onclick = () => {
+        container.classList.add('show');
+    }
+}
+
+
+export function close (btn, container){
+    btn.onclick = () => {
+        container.classList.remove('show');
+    }
+}
