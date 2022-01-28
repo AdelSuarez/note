773fabab2e8d86f56524c25fe9398002d5174453
Adel Suarez <adel90suarez@gmail.com>
2022-01-27 21:00:39 -0400

    popup buttons modularized

--- js/index.js
+++ js/index.js
@@ -1,19 +1,27 @@
+import { open, close } from "./modelWin.js";
+
 const btnOpen = document.getElementById('new_task');
 const containerNewTask = document.getElementById('container_new_task');
 const btnClose = document.getElementById('close');
-const taskForm = document.getElementById('form')
-const btnSave = document.getElementById('save')
+const taskForm = document.getElementById('form');
+const btnSave = document.getElementById('save');
+const btnNewCheckbox = document.getElementById('new-checkbox');
+const btnNewNote = document.getElementById('new-note');
 
+open( btnOpen, containerNewTask );
+close( btnClose, containerNewTask );
 
-btnOpen.onclick = () => {
-    containerNewTask.classList.add('show');
+btnNewCheckbox.onclick = () => {
+    alert('new checkbox');
 }
 
-btnClose.onclick = () => {
-    containerNewTask.classList.remove('show');
+btnNewNote.onclick = () => {
+    alert('new note');
 }
 
-btnSave.onclick = () =>{
+
+
+btnSave.onclick = () => {
     console.log(taskForm['title'].value);
     taskForm.reset();
 }
